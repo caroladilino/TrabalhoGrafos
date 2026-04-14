2af4a87a7d0f56aad85a72470eaeeb1ca77025c8
Carolina Adilino <cmqadilino@gmail.com>
2026-04-14 19:44:53 -0300

    estrutura funções da estrutura

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 // Copyright [2026] Carolina Adilino
+#include <list>
+#include <algorithm>
+
 
 //namespace = uma região do código 
 namespace structures {
-    
+
+
+    //Template = uma função sem tipo. imagina q qnd rodar o código "T" vira int ou float
     template<typename T>
     class Grafo{
         public:
@@ -22,7 +27,7 @@ namespace structures {
             int rotulo(const T& vertice);
 
             //retorna os vizinhos do vértice v
-            T vizinhos(const T& vertice);
+            std::list<T> vizinhos(const T& vertice);
 
             //se {u, v} ∈ E, retorna verdadeiro e se não existir, retorna falso
             bool haAresta(const T& vertice1, const T& vertice2);
@@ -35,10 +40,48 @@ namespace structures {
         
         private:
             std::list<T> vertices;
-            std::list<T> arestas;
+            std::list<T> arestas; //decidir como armazenar arestas 
             int funcao(std::list<T> arestas)
 
     };
+
+    //botar construtores e destrutores?
+
+    //Função retorna quantidade de vértices 
+    template<typename T>
+    int structures::Grafo<T>::qtdVertices(){
+        return vertices.size();
+    }
+
+    template<typename T>
+    int structures::Grafo<T>::qtdArestas(){
+        return arestas.size();
+    }
+
+    template<typename T>
+    int structures::Grafo<T>::grau(const T& vertice){
+        //botar codigo aqui
+    }
+
+    template<typename T>
+    int structures::Grafo<T>::rotulo(const T& vertice){
+        //botar codigo aqui
+    }
+
+    template<typename T>
+    std::list<T> structures::Grafo<T>::vizinhos(const T& vertice){
+        //botar codigo aqui
+    }
+
+    template<typename T>
+    bool structures::Grafo<T>::haAresta(const T& vertice1, const T& vertice2){
+        //botar codigo aqui
+    }
+
+    template<typename T>
+    int structures::Grafo<T>::peso(const T& vertice1, const T& vertice2){
+        //botar codigo aqui
+    }
 }
 
 
