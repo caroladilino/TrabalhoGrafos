50bc653ea6fdf5c8a2ab50cb8208e80ea6bdf2ad
Carolina Adilino <cmqadilino@gmail.com>
2026-04-25 22:56:56 -0300

    algoritmo busca em largura

--- functions.h
+++ functions.h
@@ -19,28 +19,52 @@ void buscaLargura(const structures::Grafo<T>& G, const T& s);
 template<typename T>
 void buscaLargura(const structures::Grafo<T>& G, const T& s){
 
-    std::map<T, bool> vetor_conhecido;
-    std::map<T, T> vetor_ancestral;
-    std::map<T, int> vetor_distancia;
+    std::map<T, bool> map_conhecido;
+    std::map<T, T> map_ancestral;
+    std::map<T, int> map_distancia;
     
     //configurando todos os vertices
     for (auto const& par : G.vertices){
         T v = par.first;
-        vetor_conhecido[v] = false;
-        vetor_ancestral[v];
-        vetor_distancia[v] = 10000;
+        map_conhecido[v] = false;
+        map_ancestral[v];
+        map_distancia[v] = 10000;
     }
 
     //configurando o vértice de origem
-    vetor_conhecido.at(s) = true;
-    vetor_distancia.at(s) = 0;
+    map_conhecido.at(s) = true;
+    map_distancia.at(s) = 0;
 
     //preparando a fila de visitas
     std::queue<T> Q;
     Q.push(s);
 
-    std::cout << Q.front();
-    std::cout << "buscaLargura rodou ctinho";
+    //Propagação das visitas
+    while (!Q.empty()){
+        T u = Q.front();
+        Q.pop();
+        for (const auto& v : G.vizinhos(u)){
+            if (map_conhecido.at(v) == false){
+                //conhecendo o vértice v
+                map_conhecido.at(v) = true;
+                map_distancia.at(v) = map_distancia.at(u) + 1;
+                map_ancestral.at(v) = u;
+                Q.push(v);
+            }
+        }
+    }
+
+    for (int i=0; i < (map_distancia.size() - 1); i++){
+        std::cout << i << ": ";
+        for (const auto& [key, value] : map_distancia) {
+            if (value == i){
+                std::cout << key << ",";
+            }
+        }
+        std::cout << "\n";
+    }
+
+    std::cout << "buscaLargura terminou de rodar" << "\n";
 
 
 }
